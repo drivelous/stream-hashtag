248ec0009e0f308dcd209fca00e12cc2acde62bd
Sean Dominguez <sean@drivelous.com>
2015-01-27 18:44:47 -0800

    Adds Hashtags model

--- src/catalog/models.py
+++ src/catalog/models.py
@@ -1,9 +1,12 @@
 import os
 from django.db import models
 
+class Hashtags(models.Model):
+	tag = models.CharField(max_length=50, null=False, blank=False)
+
 class Tweets(models.Model):
 	username = models.CharField(max_length=25, null=False, blank=False)
 	text = models.TextField(null=False, blank=False)
 	tweet_id = models.CharField(max_length=50, default='', null=False, blank=False)
+	# tweet_date = models.DateTimeField()
 	date = models.DateTimeField(auto_now_add=True, auto_now=False)
-	url = models.CharField(max_length= 200, null=True, blank=True)
--- src/catalog/views.py
+++ src/catalog/views.py
@@ -3,8 +3,7 @@ from django.shortcuts import render
 from.models import Tweets
 
 def stream(request):
-	all_tweets = Tweets.objects.all()
-	
+	all_tweets = Tweets.objects.all().order_by('-date')
 	return render(request, 'catalog/stream.html', {
 		'all_tweets': all_tweets,
 	})
