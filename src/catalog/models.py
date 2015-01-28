1dbfa977d9791915a10a2f32e7c8d99028882d5c
Sean Dominguez <sean@drivelous.com>
2015-01-28 13:16:42 -0800

    Adds correct tweet timestamps, orders by latest

--- src/catalog/models.py
+++ src/catalog/models.py
@@ -1,4 +1,6 @@
 import os
+from datetime import datetime
+
 from django.db import models
 
 class Hashtags(models.Model):
@@ -8,5 +10,5 @@ class Tweets(models.Model):
 	username = models.CharField(max_length=25, null=False, blank=False)
 	text = models.TextField(null=False, blank=False)
 	tweet_id = models.CharField(max_length=50, default='', null=False, blank=False)
-	# tweet_date = models.DateTimeField()
+	tweet_date = models.DateTimeField(null=False, blank=False, default=datetime.now)
 	date = models.DateTimeField(auto_now_add=True, auto_now=False)
--- src/catalog/views.py
+++ src/catalog/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 from .models import Tweets
 
 def stream(request):
-	all_tweets = Tweets.objects.all().order_by('-date')
+	all_tweets = Tweets.objects.all().order_by('-tweet_date')
 	return render(request, 'catalog/stream.html', {
 		'all_tweets': all_tweets,
 	})
