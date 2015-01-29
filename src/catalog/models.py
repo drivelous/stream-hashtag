c4fa14876d911c93954a1e986cccc9e6a979beda
Sean Dominguez <sean@drivelous.com>
2015-01-28 19:06:26 -0800

    Changes tweet_id from CharField to BigInteger

--- src/catalog/models.py
+++ src/catalog/models.py
@@ -9,6 +9,6 @@ class Hashtags(models.Model):
 class Tweets(models.Model):
 	username = models.CharField(max_length=25, null=False, blank=False)
 	text = models.TextField(null=False, blank=False)
-	tweet_id = models.CharField(max_length=50, default='', null=False, blank=False)
+	tweet_id = models.BigIntegerField(default=0, null=False, blank=False)
 	tweet_date = models.DateTimeField(null=False, blank=False, default=datetime.now)
 	date = models.DateTimeField(auto_now_add=True, auto_now=False)
